c1ca4f9380ee1d091765c80cb1fbfd314c1eafa4
Suniaster <thibousbr@gmail.com>
2018-10-22 13:30:39 -0300

    Feita lógica do winCond()

--- index.js
+++ index.js
@@ -33,6 +33,34 @@ const clicarQuadrado = (quadrado) =>
 
 const winCond = () => {
     // checa se terminou o jogo
+    // Retorna 'false' caso não haja ganhador
+    // Retorna 0 caso X tenha vencido
+    // Retorna 1 caso O tenha vencido
+    // retorna 'fim' caso o jogo tenha dado velha
+    for( i in board){
+        //Olhando Linhas onde é possivel ganhar
+        if(board[i][0]==board[i][1] && board[i][0]==board[i][2] && board[i][0]!=undefined){
+            return board[i][0];
+        }
+        //Olhando Colunas
+        if(board[0][i]==board[1][i] && board[0][i]==board[2][i] && board[0][i]!=undefined){
+            return board[0][i];
+        }
+    }
+    //Olhando diagonal principal
+    if(board[0][0]==board[1][1] && board[0][0]==board[2][2] && board[0][0]!=undefined){
+        return board[0][0];
+    }
+    //Olhando diagonal Secundaria
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[0][2]!=undefined){
+        return board[0][2];
+    }
+    if(turno == 9){
+        return 'fim';
+    }
+    else{
+        return false;
+    }
 }
 
 const resetGame = () => {
