78896ec4f59363a3777fe9aedcde25a3134a250b
Suniaster <thibousbr@gmail.com>
2018-10-22 20:33:41 -0300

    Feitas Mudanças pra adicionar interação entre mouse e circulos

--- canvas.js
+++ canvas.js
@@ -1,10 +1,26 @@
 var canvas = document.getElementById('canv');
 var c = canvas.getContext('2d');
-let g =0.1;
+let g =0.98;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
+//Interações com o mouse
+let mouse = {
+    x:undefined,
+    y:undefined,
+    dx:0,
+    dy:0
+}
+var timestamp = Date.now();
+canvas.addEventListener('mousemove',
+    function(event){
+	var now = Date.now();
+	mouse.dx = (event.x - mouse.x)/(now-timestamp);
+	mouse.dy = (event.y - mouse.y)/(now-timestamp);
+    mouse.x = event.x;
+	mouse.y = event.y;
+	timestamp = now;
+});
     /* Parte da animação das bolas */
 //Cria Variaveis e as funções que controlam a fisica dos circulos
 function Circle(x,y,dx,dy,radius){
@@ -26,8 +42,9 @@ function Circle(x,y,dx,dy,radius){
     }
 
     this.update = function(){
-        if(this.x + this.radius > canvas.width || this.x - this.radius <0){
+        if(this.x + this.radius > canvas.width || this.x - this.radius < 0){
             this.dx= -this.dx;
+            this.dx *=0.8;
         }
         if(this.y + this.radius > canvas.height && this.isnFreezed){
             this.dy= -this.dy;
@@ -37,43 +54,57 @@ function Circle(x,y,dx,dy,radius){
         }
         if(this.maxHeight > canvas.height - 2*this.radius){
             this.isnFreezed = false;
-        }
-        if(this.y + this.radius > canvas.height ){
+		}
+		else{
+			this.isnFreezed = true;
+		}
+        if(this.y + this.radius > canvas.height){
             this.dy *=0.9;        
+        }
+		if( mouse.x > this.x - this.radius &&
+			mouse.x < this.x + this.radius &&
+			mouse.y < this.y + this.radius &&
+			mouse.y > this.y - this.radius
+        ){
+			this.dx+=mouse.dx;
+			this.dy+=mouse.dy;
+			// this.x=mouse.x;
+			// this.y=mouse.y;
         }
         this.x += this.dx;
         this.y += this.dy;
-        this.dy +=g;
+        this.dy += g;
         this.draw();
     }
 }
-
+//
 let circleArray = [];
-let InitialQuant =10;
+let InitialQuant =1;
 let SpawnRate = 50;
 //Cria Bolas Espalhadas pela tela
 for(i=0;i< InitialQuant;i++){
-    var radius = (Math.random()*10)+30;
+    var radius = (Math.random()*10)+300;
     var x = Math.random() * (canvas.width - radius *2)+ radius;
     var y = Math.random() * (canvas.height-radius *2)+ radius;
     var dx = (Math.random() -0.5) * 8;
     var dy = (Math.random() - 0.5)* 0;
     circleArray.push(new Circle(x,y,dx,dy,radius));
 }
-// canvas.addEventListener('mouseover', ChangeColor);
-function animate(){
-    requestAnimationFrame(animate);
-    var Spawn;
+function SpawnCircle(){
+	var Spawn;
     Spawn = Math.random() * SpawnRate;
     if(Spawn < 1){
-        var radius = (Math.random()*10)+10;
+        var radius = (Math.random()*10)+20;
         var x = Math.random() * (canvas.width - radius *2)+ radius;
         var dx = (Math.random() -0.5) * 8;
         var dy = (Math.random() - 0.5)* 0;
         circleArray.push(new Circle(x,-radius,dx,dy,radius));
     }
+}
+function animate(){
+    requestAnimationFrame(animate);
     c.clearRect(0,0,canvas.width,canvas.height);
-    // WriteText();
+	SpawnCircle();
     for(var i = 0; i< circleArray.length;i++){
         circleArray[i].update();
         if(circleArray[i].y > canvas.height +circleArray[i].radius){
